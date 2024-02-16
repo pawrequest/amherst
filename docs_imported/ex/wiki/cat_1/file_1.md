befcf0ca0cc7c1f8f1541c1d70e2120952c9d41a
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-02-16 21:03:33 +0000

    expresslink 0.0.1

---
+++ docs_imported/ex/wiki/cat_1/file_1.md
@@ -0,0 +1,13 @@
+# Instructions File 1
+
+Here we actually write instructions using markdown or rst syntax
+
+A list of items with - or * or numbers and space at start of line
+- What To Say - Voice procedure
+- How To Say It - Voice Technique
+
+## A level 2 heading with two hashes and space at start of line
+some instructions here
+
+### A level 3 heading with three hashes and space at start of line
+more instructions here
---
+++ src/amherst/shipping/parcelforce.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+import shipr.expresslink as pf
+
+ENV_FILE = r'C:\Users\giles\prdev\am_dev\amherst\.env'
+
+load_dotenv(ENV_FILE)
+
+WSDL = os.environ.get('PF_WSDL')
+USERNAME = os.getenv('PF_EXPR_SAND_USR')
+PASSWORD = os.getenv('PF_EXPR_SAND_PWD')
+AUTH = pf.PFAuth(USERNAME, PASSWORD)
+
+
+def main():
+    config = pf.PFConfig(WSDL, AUTH)
+    pf_client = pf.PFExpressLink(config)
+    postcode = 'NW6 4TE'
+
+    pc_response = pf_client.addresses_from_postcode(postcode)
+
+    print(pc_response)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
