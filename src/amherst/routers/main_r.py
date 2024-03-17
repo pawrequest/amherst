473e568c3cc17b2e11c6edb10d83601816dd4e37
paw <pawrequest@users.noreply.github.com>
2024-03-17 21:36:00 +0000

    unnecessary long refactor wtf

--- src/amherst/routers/main_r.py
+++ src/amherst/routers/main_r.py
@@ -1,12 +1,14 @@
 import fastapi
 import fastuipr
-from fastuipr import builders, components as c, events, styles
+from fastuipr import builders, styles
+from fastui import components as c, events, FastUI
+from ..front import styles as am_styles
 
 router = fastapi.APIRouter()
 
 
 # @router.get('/')
-@router.get('/', response_model=fastuipr.FastUI, response_model_exclude_none=True)
+@router.get('/', response_model=FastUI, response_model_exclude_none=True)
 async def default_route():
     print('main route')
     # return fastapi.responses.RedirectResponse(url='/hire/view/1')
@@ -17,7 +19,7 @@ async def default_route():
                 c.Button(
                     text='go',
                     on_click=events.GoToEvent(url='/hire/view/1'),
-                    class_name=styles.BOXES_BUTTON
+                    class_name=am_styles.BOXES_BUTTON
                 ),
                 class_name=styles.ROW_STYLE,
             ),
