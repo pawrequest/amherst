bef086ca99a5e5cf6e88ce0323d4f20f72135504
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-04-08 18:59:59 +0100

    absolute imporrts

--- src/amherst/front/__init__.py
+++ src/amherst/front/__init__.py
@@ -1,10 +1,10 @@
-from .book import router as booking_router
-from .booked import router as booked_router
-from .forms import router as forms_router
-from .forms_test import router as forms_test_router
-from .ship import router as shipping_router
-from .ship_model import router as ship_model_router
-from .splash import router as splash_router
+from amherst.front.book import router as booking_router
+from amherst.front.booked import router as booked_router
+from amherst.front.forms import router as forms_router
+from amherst.front.forms_test import router as forms_test_router
+from amherst.front.ship import router as shipping_router
+from amherst.front.ship_model import router as ship_model_router
+from amherst.front.splash import router as splash_router
 
 __all__ = [
     'booked_router',
@@ -14,5 +14,4 @@ __all__ = [
     'splash_router',
     'ship_model_router',
     'forms_test_router'
-
 ]
