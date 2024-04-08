6877ba39726372b16cb2db386789d3405abb8c6b
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-04-08 18:08:33 +0100

    HOME not AMHERST address remove dict

--- src/amherst/shipper.py
+++ src/amherst/shipper.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 
 import os
 
-from shipr import ELClient, models, msgs, ship_ui
+from shipr import ELClient, msgs, ship_ui
 from shipr.models import pf_ext, pf_lists, pf_shared, pf_top
 
 
-class AmherstAddress(pf_ext.BaseAddress):
+class HomeAddress(pf_ext.BaseAddress):
     address_line1: str = '70 Kingsgate Road'
     address_line2: str = 'Kilburn'
     address_line3: str = ''
@@ -16,17 +16,7 @@ class AmherstAddress(pf_ext.BaseAddress):
     country: str = 'GB'
 
 
-am_add_dict = dict(
-    address_line1='70 Kingsgate Road',
-    address_line2='Kilburn',
-    address_line3='',
-    town='London',
-    postcode='NW6 4TE',
-    country='GB',
-)
-
-
-class AmherstContact(pf_top.Contact):
+class HomeContact(pf_top.Contact):
     business_name: str = 'Amherst Radio Center'
     email_address: str = 'radios@amherst.co.uk'
     mobile_phone: str = '07979147257'
@@ -43,7 +33,7 @@ class AmherstContact(pf_top.Contact):
 
 class AmShipper(ELClient):
     def state_to_inbound_request(self, state: ship_ui.ShipState):
-        ship_req = state_to_amherst_collection(state)
+        ship_req = state_to_inbound_shipment(state)
         req = msgs.CreateCollectionRequest(
             authentication=self.config.auth,
             requested_shipment=ship_req
@@ -54,39 +44,21 @@ class AmShipper(ELClient):
         if state.direction == 'in':
             return self.state_to_inbound_request(state)
         if state.direction == 'out':
-            return self.create_outbound_request(state)
+            return self.state_to_outbound_request(state)
         raise ValueError('Invalid direction')
 
 
-# def booking_state_to_shipment_complex(state: ship_ui.ShipState) -> pf_top.RequestedShipmentSimple:
-#     # add = elt.AddressRecipientPF.model_validate(state.address)
-#     return pf_top.RequestedShipmentSimple(
-#         department_id=s_types.DepartmentNum,
-#         shipment_type='DELIVERY',
-#         contract_number=os.environ["PF_CONT_NUM_1"],
-#         service_code=state.service,
-#         shipping_date=state.ship_date,
-#         recipient_contact=state.contact,
-#         recipient_address=state.address,
-#         total_number_of_parcels=state.boxes,
-#         sender_address=AmherstPFAddress,
-#         sender_contact=AmherstContact,
-#
-#     )
-
-
-def state_to_amherst_collection(state: ship_ui.ShipState) -> models.CollectionMinimum:
-    col_min = models.CollectionMinimum(
+def state_to_inbound_shipment(state: ship_ui.ShipState) -> pf_top.CollectionMinimum:
+    col_min = pf_top.CollectionSimple(
         contract_number=os.environ['PF_CONT_NUM_1'],
         service_code=pf_shared.ServiceCode.EXPRESS24,
         shipping_date=state.ship_date,
-        recipient_contact=AmherstContact(),
-        recipient_address=pf_ext.AddressRecipient.model_validate(am_add_dict),
+        recipient_contact=HomeContact(),
+        recipient_address=pf_ext.AddressRecipient.model_validate(HomeAddress().model_dump()),
         total_number_of_parcels=state.boxes,
         print_own_label=True,
         collection_info=pf_top.collection_info_from_state(state),
         reference_number1=state.reference,
         special_instructions1=state.special_instructions,
-
     )
     return col_min.model_validate(col_min)
