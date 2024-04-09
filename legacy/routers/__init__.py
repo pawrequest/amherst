6bdbb68699cc582f40e62c23adf92eefed501e44
Paul-Sees-Solutions <79617825+Paul-Sees-Solutions@users.noreply.github.com>
2024-04-09 16:52:59 +0100

    tidy

--- legacy/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .. import sample_data
-from amherst.front.pages.booking_route import router as booking_router
-from amherst.front.pages.forms import router as forms_router
-from amherst.front.pages.main_r import router as main_router
-from .shipping_route import router as ship_router
-from .server_load import router as server_router
-
-__all__ = ['ship_router', 'booking_router', 'sample_data', 'forms_router', 'main_router', 'server_router']
