0c4e6973dc2a4daff1db4681601e590f189b1132
Paw <PawRequest@users.noreply.github.com>
2024-07-05 17:34:48 +0100

    more fils

--- src/amherst/filters.py
+++ src/amherst/filters.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import functools
 from datetime import date, timedelta
 
+from loguru import logger
+
 from amherst.commence_adaptors import HireAliases, HireStatus, SaleAliases
 from pycommence.filters import (
     ConditionType,
@@ -43,44 +45,23 @@ def sale_date_fils(sale_start):
     return (FieldFilter(column=SaleAliases.DATE_ORDERED, condition=ConditionType.AFTER, value=to_cmc_date(sale_start)),)
 
 
-def customer_initial_array() -> FilterArray:
-    return FilterArray.from_filters(
-        ConnectedFieldFilter(
-            column='Has Hired',
-            connection_category='Hire',
-            connected_column='Send Out Date',
-            condition=ConditionType.AFTER,
-            value=to_cmc_date(INITIAL_HIRE_START),
-        ),
-        ConnectedFieldFilter(
-            column='Has Hired',
-            connection_category='Hire',
-            connected_column='Send Out Date',
-            condition=ConditionType.BEFORE,
-            value=to_cmc_date(INITIAL_HIRE_END),
-        ),
-        ConnectedFieldFilter(
-            column='Involves',
-            connection_category='Sale',
-            connected_column='Date Ordered',
-            condition=ConditionType.AFTER,
-            value=to_cmc_date(INITIAL_SALE_START),
-        ),
-        logic='And, Or',
-        sorts=CUSTOMER_SORTS,
-    )
-
-
 def cust_init_2():
     hconnect = Connection2(name='Has Hired', category='Hire', column='Name')
     sconnect = Connection2(name='Involves', category='Sale', column='Name')
-    cust_con_fils = [
+    hire_con_fils = [
         ConnectedFieldFilter.from_field_fil(f, hconnect) for f in hire_fils_initial_array().filters.values()
     ]
+    logger.info(hire_con_fils)
     sale_con_fils = [
         ConnectedFieldFilter.from_field_fil(f, sconnect) for f in sale_fils_initial_array().filters.values()
     ]
-    return FilterArray.from_filters(*cust_con_fils, *sale_con_fils, logic='And, And, And, Or', sorts=CUSTOMER_SORTS)
+    logger.info(sale_con_fils)
+    return FilterArray.from_filters(
+        *hire_con_fils,
+        # *sale_con_fils,
+        # logic='And, And, And, Or',
+        sorts=CUSTOMER_SORTS,
+    )
 
 
 def hire_fils_initial_array() -> FilterArray:
