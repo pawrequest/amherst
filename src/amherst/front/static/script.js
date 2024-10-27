ca516faf23b4c6579ec9c07b68a0977432ba6921
paw <pawrequest@users.noreply.github.com>
2024-10-27 19:21:04 +0000

    rn tocontnet

--- src/amherst/front/static/script.js
+++ src/amherst/front/static/script.js
@@ -22,3 +22,31 @@ function closePopup() {
     document.getElementById('popupDetails').style.display = 'none';
 }
 
+
+
+
+/**
+ * Populates form fields with shipment data.
+ * @param {Shipment} shipment - The shipment data.
+ */
+function populateForm(shipment) {
+    // console log the shipment data
+    console.log('Populating form from shipment data:', shipment);
+    document.getElementById('ship_date').value = shipment.ShippingDate;
+    document.getElementById('boxes').value = shipment.TotalNumberOfParcels || 1;
+    document.getElementById('business_name').value = shipment.RecipientContact.BusinessName || "";
+    document.getElementById('contact_name').value = shipment.RecipientContact.ContactName || "";
+    document.getElementById('email').value = shipment.RecipientContact.EmailAddress || "";
+    document.getElementById('mobile_phone').value = shipment.RecipientContact.MobilePhone || "";
+    document.getElementById('reference_number1').value = shipment.ReferenceNumber1 || "";
+    document.getElementById('reference_number2').value = shipment.ReferenceNumber2 || "";
+    document.getElementById('reference_number3').value = shipment.ReferenceNumber3 || "";
+    document.getElementById('special_instructions1').value = shipment.SpecialInstructions1 || "";
+    document.getElementById('special_instructions2').value = shipment.SpecialInstructions2 || "";
+    document.getElementById('special_instructions3').value = shipment.SpecialInstructions3 || "";
+    document.getElementById('address_line1').value = shipment.RecipientAddress.AddressLine1 || "";
+    document.getElementById('address_line2').value = shipment.RecipientAddress.AddressLine2 || "";
+    document.getElementById('address_line3').value = shipment.RecipientAddress.AddressLine3 || "";
+    document.getElementById('town').value = shipment.RecipientAddress.Town || "";
+    document.getElementById('postcode').value = shipment.RecipientAddress.Postcode || "";
+}
