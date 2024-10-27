9e878ada18c9386c3fe3f9a75df9fc8d45e6a26f
paw <pawrequest@users.noreply.github.com>
2024-10-27 18:27:25 +0000

    search

--- src/amherst/back/backend_pycommence.py
+++ src/amherst/back/backend_pycommence.py
@@ -13,7 +13,7 @@ from pycommence.pycmc_types import MoreAvailable
 from pycommence.pycommence_v2 import PyCommence
 from amherst.back.search_paginate import SearchRequest, SearchResponse
 from amherst.models.amherst_models import AMHERST_TABLE_MODELS
-from amherst.models.maps import AmherstTableName, record_model
+from amherst.models.maps import AmherstTableName, record_model, CMAP
 
 
 @contextlib.contextmanager
@@ -88,14 +88,10 @@ async def search_f_path(
 
 
 async def pycommence_search(
-        # request: Request,
-
-        # pycmc: PyCommence,
         search_request: SearchRequest,
-        pycmc: PyCommence = Depends(pycmc_f_path),
+        pycmc: PyCommence,
 ):
     csr = pycmc.csr(search_request.csrname)
-    # record_type: type[BaseModel] = CURSOR_MAP[search_request.csrname]['input_type']
     record_type: type[BaseModel] = await record_model(search_request.csrname)
 
     if search_request.row_id:
@@ -103,10 +99,19 @@ async def pycommence_search(
         validated = record_type.model_validate(record)
         return SearchResponse(records=[validated], more=None, search_request=search_request)
 
-    if search_request.pk_value:
-        fil = csr.pk_filter_array(pk=search_request.pk_value, condition=search_request.condition)
-        pycmc.set_csr(search_request.csrname, filter_array=fil)
+    fil = FilterArray()
+    logger.warning('this likely buggy due to logic in FilterArray')
+    if search_request.filtered:
+        fil = CMAP[search_request.csrname].default_filter
+        # def_fils = CMAP[search_request.csrname].default_filter.filters
+        # fil.add_filters(def_fils)
 
+    if search_request.pk_value:
+        pk_filter = csr.pk_filter(pk=search_request.pk_value, condition=search_request.condition)
+        fil.add_filter(pk_filter)
+        # fil = csr.pk_filter_array(pk=search_request.pk_value, condition=search_request.condition)
+
+    pycmc.set_csr(search_request.csrname, filter_array=fil)
     more, records = await gather_records(input_type=record_type, pycmc=pycmc, sq=search_request)
     return SearchResponse(records=records, more=more, search_request=search_request)
 
@@ -114,6 +119,17 @@ async def pycommence_search(
 async def pycommence_response(
         # request: Request,
         search_request: SearchRequest,
+
+) -> SearchResponse:
+    with pycommence_context(search_request.csrname) as pycmc:
+        return await pycommence_search(search_request, pycmc)
+    # return await pycommence_search(search_request)
+
+
+async def pycommence_response2(
+        # request: Request,
+        search_request: SearchRequest,
+
 
 ) -> SearchResponse:
     with pycommence_context(search_request.csrname) as pycmc:
@@ -132,3 +148,45 @@ def row_from_path_id(
         row = pycmc.read_row(id=row_id)
     # row = pycmc.read_row(id=row_id)
     return record_type.model_validate(row)
+
+
+
+async def pycommence_search2(
+        search_request: SearchRequest,
+        pycmc: PyCommence,
+        with_id: bool = True,
+) -> SearchResponse:
+    logger.debug(f'pycommence_search({search_request=}, {pycmc=})')
+    record_type: type[BaseModel] = await record_model(search_request.csrname)
+    # record_type: type[BaseModel] = CURSOR_MAP[search_request.csrname]['input_type']
+    csr = pycmc.csr(search_request.csrname)
+
+    if search_request.row_id:
+        record = pycmc.read_row(id=search_request.row_id)
+        validated = record_type.model_validate(record)
+        return SearchResponse(records=[validated], more=None, search_request=search_request)
+
+    if search_request.pk_value:
+        fil = csr.pk_filter_array(pk=search_request.pk_value, condition=search_request.condition)
+        pycmc.set_csr(search_request.csrname, filter_array=fil)
+
+    async def gather_records():
+        _records = []
+        _more = None
+        for row in pycmc.read_rows(
+                csrname=search_request.csrname,
+                with_category=True,
+                pagination=search_request.pagination,
+                with_id=with_id,
+        ):
+            if isinstance(row, MoreAvailable):
+                _more = row
+                _more.json_link = search_request.next_q_str_json
+                _more.html_link = search_request.next_q_str
+                break
+            _records.append(record_type.model_validate(row))
+        return _more, _records
+
+    more, records = await gather_records()
+    return SearchResponse(records=records, more=more, search_request=search_request)
+
