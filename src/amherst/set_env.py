b132e88e412cf64d276c5116bb52f6ea9dfad4c6
Paw <paw@pawrequest.com>
2025-04-30 17:53:19 +0100

    set envs tidy

--- src/amherst/set_env.py
+++ src/amherst/set_env.py
@@ -4,39 +4,28 @@ from pathlib import Path
 
 
 @functools.lru_cache
-def get_ampr():
-    amherstpr = os.getenv('AMHERSTPR')
-    if not amherstpr:
+def get_envs_dir():
+    envs_dir = os.getenv('AMHERSTPR')
+    if not envs_dir:
         raise ValueError('AMHERSTPR environment variable not set')
-    amherstpr = Path(amherstpr)
-    if not amherstpr.exists():
-        raise ValueError(f'{amherstpr} directory does not exist.')
-    return amherstpr
-
-
-AMHERSTPR = get_ampr()
+    envs_dir = Path(envs_dir)
+    if not envs_dir.exists():
+        raise ValueError(f'{envs_dir} directory does not exist.')
+    return envs_dir
 
 
 def set_env(filename: str, keyname: str):
-    env_file = AMHERSTPR / filename
+    env_file = get_envs_dir() / filename
     if not env_file.exists():
-        raise ValueError(f'{env_file} env file does not exist.')
+        raise ValueError(f'{env_file} does not exist.')
     os.environ[keyname.upper()] = str(env_file)
     return env_file
 
 
-def set_live_env():
-    set_env('pf.env', 'SHIP_ENV')
-    set_env('am.env', 'AM_ENV')
-
-
-def set_sandbox_env():
-    set_env('pf_sandbox.env', 'SHIP_ENV')
-    set_env('am_sandbox.env', 'AM_ENV')
-
-
 def set_amherstpr_env(sandbox: bool = False):
     if sandbox:
-        set_sandbox_env()
+        set_env('pf_sandbox.env', 'SHIP_ENV')
+        set_env('am_sandbox.env', 'AM_ENV')
     else:
-        set_live_env()
+        set_env('pf.env', 'SHIP_ENV')
+        set_env('am.env', 'AM_ENV')
