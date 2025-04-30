83d2792233355f799886aa08eeb7f22c10433438
Paw <paw@pawrequest.com>
2025-04-30 18:01:15 +0100

    config tidy

--- src/amherst/config.py
+++ src/amherst/config.py
@@ -8,15 +8,15 @@ from pathlib import Path
 import sys
 import typing as _t
 from urllib.parse import quote
+from starlette.templating import Jinja2Templates
 
 import pydantic as _p
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pawlogger import get_loguru
-from starlette.templating import Jinja2Templates
-#
-#
+
+
 @functools.lru_cache
 def get_ampr():
     amherstpr = os.getenv('AMHERSTPR')
@@ -29,25 +29,6 @@ def get_ampr():
 
 
 AMHERSTPR = get_ampr()
-#
-#
-# def set_env(filename: str, keyname: str):
-#     env_file = AMHERSTPR / filename
-#     if not env_file.exists():
-#         raise ValueError(f'{env_file} env file does not exist.')
-#     os.environ[keyname.upper()] = str(env_file)
-#     return env_file
-#
-#
-# def set_live_env():
-#     set_env('pf.env', 'SHIP_ENV')
-#     set_env('am.env', 'AM_ENV')
-#
-#
-# def set_sandbox_env():
-#     set_env('pf_sandbox.env', 'SHIP_ENV')
-#     set_env('am_sandbox.env', 'AM_ENV')
-
 
 def set_src_dir(v, values):
     # pyinstaller compatibility
@@ -66,7 +47,8 @@ def src_dir_no_exec(v, values):
 class Settings(BaseSettings):
     # db_loc: Path
     log_file: Path
-    src_dir: _t.Annotated[Path, _p.BeforeValidator(set_src_dir)] = None
+    src_dir: Path = Path(__file__).resolve().parent
+    # src_dir: _t.Annotated[Path, _p.BeforeValidator(set_src_dir)] = None
     # data_dir: Path = Path(__file__).parent / '_data'
     templates: Path | None = None
     log_level: str = 'INFO'
