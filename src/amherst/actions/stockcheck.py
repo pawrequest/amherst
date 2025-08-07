a121a10f612f70b38387cb10907c6985f40d1306
Paw <paw@pawrequest.com>
2025-08-07 17:10:16 +0100

    stockcheck anything

--- src/amherst/actions/stockcheck.py
+++ src/amherst/actions/stockcheck.py
@@ -4,42 +4,34 @@ import functools
 import time
 from collections.abc import Generator
 from datetime import date, timedelta
+from enum import StrEnum
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from pawlogger import get_loguru
-from pycommence.filters import FieldFilter, FilterArray
+from pycommence.filters import ConditionType, FieldFilter, FilterArray
 from pycommence.pycmc_types import CmcDateFormat, Pagination, RowData
 from pycommence.pycommence import PyCommence
 
-from amherst.actions.one_shots import good_hires_fils
-from amherst.models.commence_adaptors import HireAliases, RadioType
+from amherst.models.commence_adaptors import HireAliases, HireStatus, RadioType
 from amherst.models.filters import HIRE_ARRAY_LOOSE
 
 logger = get_loguru(profile='local')
 
-
-def prep_df(records):
-    df = pd.DataFrame(records)
-    df[HireAliases.SEND_DATE] = pd.to_datetime(df[HireAliases.SEND_DATE], format=CmcDateFormat, errors='coerce').dt.date
-    df[HireAliases.DUE_BACK_DATE] = pd.to_datetime(
-        df[HireAliases.DUE_BACK_DATE], format=CmcDateFormat, errors='coerce'
-    ).dt.date
-    df[HireAliases.UHF] = df[HireAliases.UHF].astype(int)
-    return df
-
-
-# def prep_df(records):
+#
+# def prep_dataframe(records):
 #     df = pd.DataFrame(records)
-#     df[HireAliases.SEND_DATE] = pd.to_datetime(df[HireAliases.SEND_DATE], format=CmcDateFormat, errors='coerce')
-#     df[HireAliases.DUE_BACK_DATE] = pd.to_datetime(df[HireAliases.DUE_BACK_DATE], format=CmcDateFormat, errors='coerce')
+#     df[HireAliases.SEND_DATE] = pd.to_datetime(df[HireAliases.SEND_DATE], format=CmcDateFormat, errors='coerce').dt.date
+#     df[HireAliases.DUE_BACK_DATE] = pd.to_datetime(
+#         df[HireAliases.DUE_BACK_DATE], format=CmcDateFormat, errors='coerce'
+#     ).dt.date
 #     df[HireAliases.UHF] = df[HireAliases.UHF].astype(int)
 #     return df
 
 
 def daterang_gen(start_date, end_date) -> Generator[date, None, None]:
-    for x in range((end_date - start_date).days + 1):
-        thisdate = start_date + timedelta(days=x)
+    for delta in range((end_date - start_date).days + 1):
+        thisdate = start_date + timedelta(days=delta)
         yield thisdate
 
 
@@ -47,35 +39,59 @@ class StockChecker:
     def __init__(
         self,
         pycmc=None,
-        radiotype: RadioType = RadioType.HYTERA,
+        radiotype: RadioType = None,
         start_date: date = date.today(),
         end_date: date = date.today() + timedelta(days=6),
         stock: int = 500,
+        column_to_count: StrEnum = HireAliases.UHF,
     ):
+        if radiotype is None and column_to_count == HireAliases.UHF:
+            raise ValueError(
+                'You must provide a radiotype when checking radio stock. '
+                'Use the RadioType enum to specify the type.'
+            )
         self.radiotype = radiotype
+        self.column_to_count = column_to_count
         self.stock = stock
         self.date_range_gen = daterang_gen(start_date, end_date)
         self.pycommence = pycmc or PyCommence.with_csr('Hire')
-        self.data = self._prepare_data()
+        self.records = self.get_cmc_data()
+        self.data = self.prepare_dataframe()
 
-    def _prepare_data(self):
+    def prepare_dataframe(self):
+        df = pd.DataFrame(self.records)
+        df[HireAliases.SEND_DATE] = pd.to_datetime(
+            df[HireAliases.SEND_DATE], format=CmcDateFormat, errors='coerce'
+        ).dt.date
+        df[HireAliases.DUE_BACK_DATE] = pd.to_datetime(
+            df[HireAliases.DUE_BACK_DATE], format=CmcDateFormat, errors='coerce'
+        ).dt.date
+        # df[HireAliases.UHF] = df[HireAliases.UHF].astype(int)
+        df[self.column_to_count] = df[self.column_to_count].astype(int)
+        return df
+
+    def get_cmc_data(self):
         pag = Pagination(limit=0, offset=0)
-        filter_array = FilterArray.from_filters(*good_hires_fils())
-        rad_fil = FieldFilter(column=HireAliases.RADIO_TYPE, value=self.radiotype)
-        filter_array.add_filter(rad_fil)
+        filter_array = self.get_filter_array()
         records = [
             _.data
             for _ in self.pycommence.read_rows(csrname='Hire', filter_array=filter_array, pagination=pag)
             if isinstance(_, RowData)
         ]
-        df = prep_df(records)
-        return df
+        return records
 
-    @property
-    def filters(self):
-        # todo break this
-        return HIRE_ARRAY_LOOSE
-        # return get_filter_array('Hire')
+    def get_filter_array(self):
+        filter_array = FilterArray.from_filters(
+            FieldFilter(column=HireAliases.STATUS, condition=ConditionType.NOT_EQUAL, value=HireStatus.CANCELLED),
+            FieldFilter(column=HireAliases.STATUS, condition=ConditionType.NOT_EQUAL, value=HireStatus.RTN_OK),
+            FieldFilter(column=HireAliases.STATUS, condition=ConditionType.NOT_EQUAL, value=HireStatus.RTN_PROBLEMS),
+            FieldFilter(column=HireAliases.STATUS, condition=ConditionType.NOT_EQUAL, value=HireStatus.PACKED),
+            # FieldFilter(column=HireAliases.RADIO_TYPE, value=self.radiotype),
+        )
+        if self.radiotype:
+            filter_array.add_filter(FieldFilter(column=HireAliases.RADIO_TYPE, value=self.radiotype))
+
+        return filter_array
 
     def run(self):
         dates = list(self.date_range_gen)
@@ -93,45 +109,20 @@ class StockChecker:
 
         self.plot_data(data_df)
 
-    # def run(self):
-    #     dates = list(self.date_range_gen)
-    #     send_data = [self.to_send(datecheck) for datecheck in dates]
-    #     return_data = [self.to_return(datecheck) for datecheck in dates]
-    #
-    #     stock_levels = []
-    #     current_stock = self.stock
-    #     for send, ret in zip(send_data, return_data):
-    #         current_stock = current_stock - send + ret
-    #         stock_levels.append(current_stock)
-    #
-    #     data_df = pd.DataFrame({'Date': dates, 'Send': send_data, 'Return': return_data, 'Stock': stock_levels})
-    #
-    #     self.plot_data(data_df)
+    # def to_send1(self, datecheck: date):
+    #     filtered_data = self.data[self.data[HireAliases.SEND_DATE] == datecheck]
+    #     return filtered_data[HireAliases.UHF].sum()
+
     def to_send(self, datecheck: date):
         filtered_data = self.data[self.data[HireAliases.SEND_DATE] == datecheck]
-        return filtered_data[HireAliases.UHF].sum()
+        return filtered_data[self.column_to_count].sum()
 
     def to_return(self, datecheck: date):
         filtered_data = self.data[self.data[HireAliases.DUE_BACK_DATE] == datecheck]
-        return filtered_data[HireAliases.UHF].sum()
+        return filtered_data[self.column_to_count].sum()
 
-    # def to_return(self, datecheck: date):
-    #     filtered_data = self.data[self.data[HireAliases.DUE_BACK_DATE].dt.date == datecheck]
-    #     return filtered_data[HireAliases.UHF].sum()
-
-    #
-    # def run(self):
-    #     dates = list(self.date_range_gen)
-    #     send_data = [self.to_send(datecheck) for datecheck in dates]
-    #     rads_in = [self.how_many_in(datecheck) for datecheck in dates]
-    #     rads_out = [self.how_many_out(datecheck) for datecheck in dates]
-    #
-    #     data_df = pd.DataFrame({'Date': dates, 'Send': send_data, 'In': rads_in, 'Out': rads_out})
-    #
-    #     self.plot_data(data_df)
-
-    # def to_send(self, datecheck: date):
-    #     filtered_data = self.data[(self.data[HireAliases.SEND_DATE].dt.date == datecheck)]
+    # def to_return1(self, datecheck: date):
+    #     filtered_data = self.data[self.data[HireAliases.DUE_BACK_DATE] == datecheck]
     #     return filtered_data[HireAliases.UHF].sum()
 
     @functools.lru_cache
@@ -140,8 +131,18 @@ class StockChecker:
             (self.data[HireAliases.SEND_DATE].dt.date < datecheck)
             & (self.data[HireAliases.DUE_BACK_DATE].dt.date > datecheck)
         ]
-        rads_out = filtered_data[HireAliases.UHF].sum()
+        rads_out = filtered_data[self.column_to_count].sum()
         return rads_out
+
+    #
+    # @functools.lru_cache
+    # def how_many_out(self, datecheck):
+    #     filtered_data = self.data[
+    #         (self.data[HireAliases.SEND_DATE].dt.date < datecheck)
+    #         & (self.data[HireAliases.DUE_BACK_DATE].dt.date > datecheck)
+    #     ]
+    #     rads_out = filtered_data[HireAliases.UHF].sum()
+    #     return rads_out
 
     def how_many_in(self, datecheck: date, stock: int = None):
         stock = stock or self.stock
@@ -181,8 +182,11 @@ if __name__ == '__main__':
         stock=20,
         start_date=date(2025, 8, 7),
         end_date=date(2025, 9, 21),
+        column_to_count=HireAliases.UHF_6WAY,
     )
     sc.run()
     endtime = time.perf_counter()
     print(f'THERE WERE {len(sc.data)} RECORDS')
     print(f'Elapsed time: {endtime - starttime}')
+
+
