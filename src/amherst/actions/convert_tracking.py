ce667f17887aefc851dd6afec5ee1a417e13e98c
Paw <paw@pawrequest.com>
2025-08-13 16:50:17 +0100

    add convert tracking

--- src/amherst/actions/convert_tracking.py
+++ src/amherst/actions/convert_tracking.py
@@ -9,9 +9,15 @@ def convert_parcelforce_tracking_to_royal_mail(old_track_url: str):
     # if not old_track_url[1].lower() == 'b':
     #     raise ValueError('No "B" in string pos 2 - is this a tracking number?')
     track_num = old_track_url.split('=')[1]
+    # if track_num[0:2].lower() in ['zo', 'sp']:
+    track_num = f'PB{track_num}'
     track_num = f'{track_num}001'
-    track_num = f'{RM_URL}{track_num}'
-    print(track_num)
-    webbrowser.open(track_num, new=2)
 
+    new_track_url = f'{RM_URL}{track_num}'
+    print(new_track_url)
 
+    webbrowser.open(new_track_url, new=2)
+
+if __name__ == '__main__':
+    url = 'https://www.parcelforce.com/track-trace?trackNumber=UK3467769'
+    convert_parcelforce_tracking_to_royal_mail(url)
