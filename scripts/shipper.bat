5f14853ee66fac6ba6f8b10a4c11ff7de6f108a2
Paw <paw@pawrequest.com>
2025-09-09 10:44:46 +0100

    print shipper error message

--- scripts/shipper.bat
+++ scripts/shipper.bat
@@ -4,6 +4,7 @@ call %thisDir%/install/clone_or_pull_amherstpr_repo.bat
 pushd %AMHERSTPR%
 uv run shipper %1 %2 %3
 if %errorlevel% neq 0 (
+    echo Error: shipper exited with code %errorlevel%
     pause
 )
 popd
