0e78c9b4b7c964553884295eab2a8c4e4bcf0757
paw <pawrequest@users.noreply.github.com>
2025-09-30 23:35:18 +0100

    notes

--- scripts/install/install_amherst_envs.bat
+++ scripts/install/install_amherst_envs.bat
@@ -4,5 +4,5 @@ echo Configuring Environment
 set ENV_DIR_REMOTE="R:\paul_r\.internal\envs\v2"
 set ENV_DIR_LOCAL=%AMHERSTSHIPPER%\envs
 
-echo Copying all files from remote directory to local directory
+echo Copying all files from remote directory to local directory (%ENV_DIR_REMOTE% to %ENV_DIR_LOCAL%)
 xcopy /Y /E %ENV_DIR_REMOTE%\* %ENV_DIR_LOCAL%\
