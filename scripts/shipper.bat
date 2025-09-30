b6685af3386026d2a0d16b485b68e55e8c5486ca
paw <pawrequest@users.noreply.github.com>
2025-09-30 22:51:42 +0100

    installer check env

--- scripts/shipper.bat
+++ scripts/shipper.bat
@@ -1,7 +1,16 @@
 @echo off
+if "%AMHERSTSHIPPER%"=="" (
+    echo Error: AMHERSTSHIPPER environment variable is not set.
+    exit /b 1
+)
+echo %AMHERSTSHIPPER% is set to %AMHERSTSHIPPER%
+
 set thisDir=%~dp0
+echo this dir is %thisDir%
 call %thisDir%/install/clone_or_pull_amherst_repo.bat
+
 pushd %AMHERSTSHIPPER%
+echo Running shipper from %~dp0 with arguments: %1 %2 %3
 uv run shipper %1 %2 %3
 if %errorlevel% neq 0 if %errorlevel% neq 15 (
     echo Error: shipper exited with code %errorlevel%
