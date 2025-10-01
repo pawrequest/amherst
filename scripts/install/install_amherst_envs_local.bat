d7119431014bb02f2783652d0644edf6c6921183
paw <pawrequest@users.noreply.github.com>
2025-10-01 01:39:33 +0100

    local pp

--- scripts/install/install_amherst_envs_local.bat
+++ scripts/install/install_amherst_envs_local.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Configuring Environment
-set ENV_DIR_REMOTE=R:\paul_r\.internal\envs\v2
+set ENV_DIR_REMOTE=C:\prdev\envs\amdev
 set ENV_DIR_LOCAL=%AMHERSTSHIPPER%\envs
 
 echo Copying all files from remote directory to local directory (%ENV_DIR_REMOTE% to %ENV_DIR_LOCAL%)
---
+++ scripts/install/install_amherst_local.bat
@@ -0,0 +1,13 @@
+@echo off
+set thisDir=%~dp0
+set AMHERSTSHIPPER=C:\ProgramData\AmherstShipper
+setx AMHERSTSHIPPER %AMHERSTSHIPPER%
+
+echo Installing Amherst Shipper into %AMHERSTSHIPPER%
+
+call %thisDir%/install_git_uv.bat
+call %thisDir%/clone_or_pull_amherst_repo.bat
+call %thisDir%/install_amherst_envs_local.bat
+
+pushd %AMHERSTSHIPPER%
+uv sync --no-dev
