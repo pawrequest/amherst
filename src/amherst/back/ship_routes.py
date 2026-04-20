91192422941372fee9235acc5f8016fab33bdbb8
Paw <paw@pawrequest.com>
2026-04-20 22:09:08 +0100

    add version notification to shipping alerts

--- src/amherst/back/ship_routes.py
+++ src/amherst/back/ship_routes.py
@@ -13,6 +13,29 @@ from amherst.models.amherst_models import AmherstHire, AmherstShipableBase
 router = APIRouter()
 
 
+def get_version():
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        return version('amherst')
+    except PackageNotFoundError:
+        return 'unknown'
+
+
+async def notify_version(request):
+    alerts = Alerts.empty()
+    sandbox = request.app.shipaw_settings.shipper_live is False
+    live_str = 'sandbox' if sandbox else 'live'
+    msg = f'AmherstShipper v{get_version()} is {live_str}'
+    if sandbox:
+        notification_type = AlertType.WARNING
+    else:
+        notification_type = AlertType.NOTIFICATION
+    logger.info(msg)
+    alerts += Alert(message=msg, type=notification_type)
+    return alerts
+
+
 @router.get('/ship_form_am', response_class=HTMLResponse)
 async def get_shipping_form(
     request: Request,
@@ -29,6 +52,9 @@ async def get_shipping_form(
         msg = f'"Parcelforce" not in delivery_method: {record.delivery_method}'
         logger.warning(msg)
         alerts += Alert(message=msg, type=AlertType.WARNING)
+
+    # alerts += Alert(message='AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', type=AlertType.NOTIFICATION)
+    alerts += await notify_version(request)
 
     shipment = record.shipment()
     request.app.callback = cmc_callback
