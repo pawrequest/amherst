8953e8a0f70c39a0288aff79395e46ad034836c3
Paw <paw@pawrequest.com>
2026-04-22 22:18:08 +0100

    no tag

--- scripts/shipper.bat
+++ scripts/shipper.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Running shipper from %~dp0 with arguments: %1 %2
-uvx --from "git+https://github.com/pawrequest/amherst.git@latest" shipper %1 %2
+uvx --from "git+https://github.com/pawrequest/amherst.git" shipper %1 %2
 if %errorlevel% neq 0 if %errorlevel% neq 15 (
     echo Error: shipper exited with code %errorlevel%
     pause
