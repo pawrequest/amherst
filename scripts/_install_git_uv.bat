b188059d0af086bd396b9338445352825f47f4f5
Paw <paw@pawrequest.com>
2026-04-22 23:16:32 +0100

    script tidy

--- scripts/_install_git_uv.bat
+++ scripts/_install_git_uv.bat
@@ -2,19 +2,15 @@
 set UV_DIR=%USERPROFILE%\.local\bin
 
 REM maybe install uv
-if exist "%UV_DIR%\uv.exe" (
-    echo UV already installed
-) else (
-    echo Installing UV...
+if not exist "%UV_DIR%\uv.exe" (
+    echo Astral uv not installed - fetching from https://astral.sh/uv/install.ps1
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
     echo UV installed
 )
 
 REM maybe install git
 git --version >nul 2>&1
-if %errorlevel% equ 0 (
-    echo Git is installed.
-) else (
-    echo Installing Git.
+if %errorlevel% neq 0 (
+    echo git is not installed - installing from R:\paul_r\dist\Git-2.49.0-64-bit.exe
     call "R:\paul_r\dist\Git-2.49.0-64-bit.exe"
 )
--- scripts/install_shipper.bat
+++ scripts/install_shipper.bat
@@ -1,22 +1,23 @@
 @echo off
 set thisDir=%~dp0
-set INSTALL_DIR=C:\ProgramData\AmherstShipper
+set LOCAL_DIR=C:\ProgramData\AmherstShipper
+set REMOTE_DIR=R:\paul_r\v3\
 
-echo Installing Amherst Shipper to %INSTALL_DIR%...
+echo Installing Amherst Shipper Configuration to %LOCAL_DIR%...
 
+rem install git and uv
 call %thisDir%\_install_git_uv.bat
-call %thisDir%\_copy_envs.bat %INSTALL_DIR%
+
+rem copy envs and scripts from network location to local directory
+echo Copying envs and scripts from %REMOTE_DIR%
+xcopy /Y /E %REMOTE_DIR%\* %LOCAL_DIR%\
 
 REM Set and export environment variables
-set AMHERST_ENV=%INSTALL_DIR%\envs\amherst.env
-set SHIPAW_ENV=%INSTALL_DIR%\envs\shipaw.env
+set AMHERST_ENV=%LOCAL_DIR%\envs\amherst.env
+set SHIPAW_ENV=%LOCAL_DIR%\envs\shipaw.env
 setx AMHERST_ENV %AMHERST_ENV%
 setx SHIPAW_ENV %SHIPAW_ENV%
 
-if %errorlevel% neq 0 (
-    echo Error: shipper exited with code %errorlevel%
-    pause
-)
 echo Environment variables set:
 echo AMHERST_ENV=%AMHERST_ENV%
 echo SHIPAW_ENV=%SHIPAW_ENV%
