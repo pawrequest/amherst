7314d7d2966531ac812e3f2065d3451050539a2b
paw <pawrequest@users.noreply.github.com>
2026-05-04 14:52:08 +0100

    sync pycmc row

---
+++ src/amherst/amherst_pycmc.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from amherst_core.consts_enums import CategoryName
+from pycommence import PyCommence
+from pycommence.core.row_data import RowData
+
+
+async def row_from_pycommence(category: CategoryName, record_name: str) -> RowData:
+    with PyCommence(category) as pycmc:
+        return pycmc.item_read_csr(pk=record_name)
+
+def row_from_pycommence_sync(category: CategoryName, record_name: str) -> RowData:
+    with PyCommence(category) as pycmc:
+        return pycmc.item_read_csr(pk=record_name)
